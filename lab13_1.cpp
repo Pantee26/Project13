4c14bc5eaaf7a381671dc13c9903a738bff10824
pantee26 <panteezaza@gmail.com>
2024-12-31 15:48:46 +0700

    completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,35 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[], int N, double B[]) {
+    double sum = 0.0;     
+    double product = 1.0;  
+    double harmonic_sum = 0.0;  
+    double max_val = A[0];  
+    double min_val = A[0];  
+
+    for (int i = 0; i < N; ++i) {
+        sum += A[i];  
+        product *= A[i];  
+        harmonic_sum += 1.0 / A[i];  
+        if (A[i] > max_val) max_val = A[i];  
+        if (A[i] < min_val) min_val = A[i];  
+    }
+
+    B[0] = sum / N;  
+    B[1] = 0.0;  
+    
+    double variance = 0.0;
+    for (int i = 0; i < N; ++i) {
+        variance += pow(A[i] - B[0], 2);  
+    }
+    B[1] = sqrt(variance / N);  
+
+    B[2] = pow(product, 1.0 / N);  
+
+    B[3] = N / harmonic_sum;  
+
+    B[4] = max_val;  
+    B[5] = min_val;  
+}
